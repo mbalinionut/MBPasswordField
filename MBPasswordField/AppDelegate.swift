3b0fbc7577a82a8cf45b477519cc8d7d8a34bcc5
Alin Golumbeanu <alin@minglebit.com>
2017-11-08 15:31:49 +0200

    Initial Commit

---
+++ MBPasswordField/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MBPasswordField
+//
+//  Created by Alin Golumbeanu on 08/11/2017.
+//  Copyright © 2017 MingleBit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MBPasswordField/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MBPasswordField
+//
+//  Created by Alin Golumbeanu on 08/11/2017.
+//  Copyright © 2017 MingleBit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
